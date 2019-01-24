4c50cf644b1d9a962e2495d22e303c36145f2ab1
Filip Krakowski <Filip.Krakowski@Uni-Duesseldorf.de>
2019-01-24 17:05:42 +0100

    Add more fields to BoxedCollection

--- demo/src/test/java/de/hhu/bsinfo/autochunk/demo/data/BoxedCollection.java
+++ demo/src/test/java/de/hhu/bsinfo/autochunk/demo/data/BoxedCollection.java
@@ -22,6 +22,54 @@ public class BoxedCollection {
 
     private final Byte[] m_bytes = new Byte[] { 0xA, 0xB, 0xC, 0xD, 0xE, 0xF };
 
+    private final Byte[] m_byteArray = new Byte[] { 0xA, 0xB, 0xC, 0xD, 0xE, 0xF };
+
+    private final Character[] m_charArray = new Character[] { 'T', 'E', 'S', 'T', 'C', 'H', 'A', 'R' };
+
+    private final Short[] m_shortArray = new Short[] { 1, 2, 4, 8, 16, 32, 64, 128, 256 };
+
+    private final Integer[] m_intArray = new Integer[] { 512, 1024, 2048, 4096, 8192 };
+
+    private final Long[] m_longArray = new Long[] { 0xAAAAAAAAL, 0xBBBBBBBBL, 0xCCCCCCCCL, 0xDDDDDDDDL, 0xEEEEEEEEL };
+
+    private final Float[] m_floatArray = new Float[] { 0.5F, 1.0F, 1.5F, 2.0F, 2.5F, 3.0F, 3.5F };
+
+    private final Double[] m_doubleArray = new Double[] { 0.001, 9.999, 1.001, 10.999, 2.001, 11.999 };
+
+    private final Boolean[] m_booleanArray = new Boolean[] { true, false, true, false, true, false, true, false };
+
+    public Byte[] getByteArray() {
+        return m_byteArray;
+    }
+
+    public Character[] getCharArray() {
+        return m_charArray;
+    }
+
+    public Short[] getShortArray() {
+        return m_shortArray;
+    }
+
+    public Integer[] getIntArray() {
+        return m_intArray;
+    }
+
+    public Long[] getLongArray() {
+        return m_longArray;
+    }
+
+    public Float[] getFloatArray() {
+        return m_floatArray;
+    }
+
+    public Double[] getDoubleArray() {
+        return m_doubleArray;
+    }
+
+    public Boolean[] getBooleanArray() {
+        return m_booleanArray;
+    }
+
     public Byte[] getBytes() {
         return m_bytes;
     }
@@ -75,7 +123,15 @@ public class BoxedCollection {
                 m_float.equals(that.m_float) &&
                 m_double.equals(that.m_double) &&
                 m_boolean.equals(that.m_boolean) &&
-                Arrays.equals(m_bytes, that.m_bytes);
+                Arrays.equals(m_bytes, that.m_bytes) &&
+                Arrays.equals(m_byteArray, that.m_byteArray) &&
+                Arrays.equals(m_charArray, that.m_charArray) &&
+                Arrays.equals(m_shortArray, that.m_shortArray) &&
+                Arrays.equals(m_intArray, that.m_intArray) &&
+                Arrays.equals(m_longArray, that.m_longArray) &&
+                Arrays.equals(m_floatArray, that.m_floatArray) &&
+                Arrays.equals(m_doubleArray, that.m_doubleArray) &&
+                Arrays.equals(m_booleanArray, that.m_booleanArray);
     }
 
     @Override
